d153dfe03c763063891b4aea373950e2fc7ad38c
Chandra Sekhar Mullu <47244469+iamknownstranger@users.noreply.github.com>
2021-09-06 21:49:01 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,32 +7,35 @@ import pandas as pd
 st.header("**Emotionify** Facial Emotion Detection")
 uploaded_file = st.file_uploader(label="Upload your image to analyze the emotion", type=['png', 'jpg', 'jpeg'])
 detector = FER(mtcnn=True)
-if uploaded_file is not None:
-    print(uploaded_file)
-    if 'image' in uploaded_file.type:
-        img = mpimg.imread(uploaded_file)
+try:
+    if uploaded_file is not None:
+        print(uploaded_file)
+        if 'image' in uploaded_file.type:
+            img = mpimg.imread(uploaded_file)
 
-        st.header("You're **{}**".format(detector.top_emotion(img)[0]))
-        emotions = detector.detect_emotions(img)[0]['emotions']
-        
-        st.bar_chart(pd.DataFrame.from_dict(emotions, orient='index'))
-        st.image(img)
-    
-    if 'video' in uploaded_file.type:
+            st.header("You're **{}**".format(detector.top_emotion(img)[0]))
+            emotions = detector.detect_emotions(img)[0]['emotions']
 
-        video = Video(uploaded_file)
+            st.bar_chart(pd.DataFrame.from_dict(emotions, orient='index'))
+            st.image(img)
 
-        # Output list of dictionaries
-        raw_data = video.analyze(detector, display=False)
+        if 'video' in uploaded_file.type:
 
-        # Convert to pandas for analysis
-        df = video.to_pandas(raw_data)
-        df = video.get_first_face(df)
-        df = video.get_emotions(df)
-        st.video(uploaded_file)
+            video = Video(uploaded_file)
 
-        # Plot emotions
-        st.write(df.plot())
-        # plt.show()
+            # Output list of dictionaries
+            raw_data = video.analyze(detector, display=False)
+
+            # Convert to pandas for analysis
+            df = video.to_pandas(raw_data)
+            df = video.get_first_face(df)
+            df = video.get_emotions(df)
+            st.video(uploaded_file)
+
+            # Plot emotions
+            st.write(df.plot())
+            # plt.show()
+catch Exception:
+    st.write("Whoops! Something went wrong")
         
 st.write("Stay Tuned! **Moodify** is in progress 💕")
